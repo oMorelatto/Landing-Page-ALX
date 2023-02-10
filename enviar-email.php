a610f434cd6d17cc3cf2bc6dca090903f10d1cf2
Rogério Morelatto Filho <99264550+oMorelatto@users.noreply.github.com>
2023-02-10 16:45:28 +0000

    atualiza email

---
+++ enviar-email.php
@@ -0,0 +1,42 @@
+<?php
+
+// Valores recebidos do formulário
+
+$nome = $_POST['nomesobrenome'];
+$email = $_POST['email'];
+$telefone = $_POST['telefone'];
+
+
+
+// Corpo do e-mail
+
+$arquivo = "
+  <html>
+        <p><b>Nome: </b>$nome</p>
+        <p><b>E-mail: </b>$email</p>
+        <p><b>Telefone: </b>$telefone</p>
+  <html>
+";
+
+ 
+
+//email para quem será enviado o formulário
+$destino = "rogeriomorelatto@gmail.com";
+$assunto = "assunto: $assunto";
+
+
+
+
+
+  //Este sempre deverá existir para garantir a exibição correta dos caracteres
+  $headers  = "MIME-Version: 1.0\n";
+  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
+  $headers .= "From: $nome <$email>";
+
+  //Enviar
+  mail($destino, $assunto, $arquivo, $headers);
+  
+  echo "<meta http-equiv='refresh' content='10;URL=../contato.html'>";
+
+
+?>
